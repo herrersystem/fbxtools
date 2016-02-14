ce66ccdf4e83676dbbc64d943625e89d1f0dce76
herrersystem <anton.millescamps@gmail.com>
2016-02-14 16:37:37 +0100

    add ftp manipulation

---
+++ fbxtools/ftp.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+'''
+Freebox OS API
+for more infos : http://dev.freebox.fr/sdk/os/ftp/
+'''
+import requests
+import json
+
+
+def get_config(app):
+	'''
+	GET /api/v3/ftp/config/
+	'''
+	config=False
+	
+	r=requests.get(
+		'http://mafreebox.freebox.fr{}ftp/config/'.format(
+			app.api_base_url
+		), 
+		headers={'X-Fbx-App-Auth': app.session_token}
+	)
+	
+	response=r.json()
+	
+	if response['success']:
+		try:
+			config=response['result']
+		except KeyError:
+			config=None
+	
+	return config
+
+
+def update_config(app, config):
+	'''
+	PUT /api/v3/ftp/config/
+	
+	dict() config (example):
+	{
+		"enabled": True
+	}
+	'''
+	update=False
+	
+	if not app.AUTH_SETTINGS:
+		print('[fbx-tools] > Not Allowed [SETTINGS]')
+		return -1
+		
+	r=requests.put(
+		'http://mafreebox.freebox.fr{}ftp/config/'.format(
+			app.api_base_url
+		), 
+		headers={'content-type': 'application/json','X-Fbx-App-Auth': app.session_token},
+		data=json.dumps(config)
+	)
+	
+	response=r.json()
+	
+	if response['success']:
+		try:
+			update=response['result']
+		except KeyError:
+			update=None
+	
+	return update
