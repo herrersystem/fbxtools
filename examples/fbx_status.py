2896e58357481435230722449890ee83a5694419
Alain FERRARO <afer92@hotmail.com>
2017-02-21 23:17:30 +0100

    Create fbx_status.py

---
+++ examples/fbx_status.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+## Initialize and connect app.
+app = Fbx('http://192.168.0.254')
+app.get_session_token()
+
+
+from fbxtools.fbx import Fbx
+
+@app.api.call('/api/v3/system/')
+def get_system():
+	'''
+	GET /api/v3/system/
+	''' 
+	return {}
+
+if __name__ == "__main__":
+	resp = get_system()['data']
+	if resp['success']:
+		print (resp['result'])
+	else:
+		print (resp)
+	
