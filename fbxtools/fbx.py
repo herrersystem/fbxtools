1d14da9bfdceba89ab8e52843f16f2bef8da6f94
Alain FERRARO <afer92@hotmail.com>
2017-02-22 21:57:37 +0100

    Update fbx.py

--- fbxtools/fbx.py
+++ fbxtools/fbx.py
@@ -26,7 +26,7 @@ class Fbx():
 
 
 	def init_app(self, infos):
-		@self.api.call('/api/v3/login/authorize/', method='POST')
+		@self.api.call('/login/authorize/', method='POST')
 		def wrapper(infos):
 			return {'data': infos, 'is_json': True}
 
@@ -34,7 +34,7 @@ class Fbx():
 
 
 	def connect_app(self, app_token, app_id, challenge):
-		@self.api.call('/api/v3/login/session/', method='POST')
+		@self.api.call('/login/session/', method='POST')
 		def wrapper(app_token, app_id, challenge):
 			h = hmac.new(app_token.encode(), challenge, sha1)
 			password = h.hexdigest()
@@ -52,7 +52,7 @@ class Fbx():
 
 
 	def get_challenge(self, track_id):
-		@self.api.call('/api/v3/login/authorize/:id')
+		@self.api.call('/login/authorize/:id')
 		def wrapper(track_id):
 			args = {'id': track_id}
 
@@ -94,7 +94,7 @@ class Fbx():
 		return session_token
 
 	def get_status(self, track_id):
-		@self.api.call('/api/v3/login/authorize/:id')
+		@self.api.call('/login/authorize/:id')
 		def wrapper(track_id):
 			args = {'id': track_id}
 			return {'args': args}
