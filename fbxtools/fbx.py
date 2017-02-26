afb992eb8b52214f06c0d84a37dad2874fac4724
Alain FERRARO <afer92@hotmail.com>
2017-02-26 19:25:04 +0100

    Update fbx.py

--- fbxtools/fbx.py
+++ fbxtools/fbx.py
@@ -413,7 +413,7 @@ class Fbx():
 	
 	def set_number(self,number_id,numberinfos):
 		numberinfos.id = None
-		infosdict = numberinfos.fbobj2dict().
+		infosdict = numberinfos.fbobj2dict()
 		print(infosdict)
 		data = self._set_number(number_id,infosdict)['data']
 		#print(data)
