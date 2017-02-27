730decc4f4a8391c8bb6a03c77b5b92645b02f1f
Alain FERRARO <afer92@hotmail.com>
2017-02-27 10:48:17 +0100

    Update fbx_contact.py

--- examples/fbx_contact.py
+++ examples/fbx_contact.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-from fbxtools.fbx import Fbx,Contact
+from fbxtools.fbx import Fbx,Contact,Number
 
 ## Initialize and connect app.
 app = Fbx('http://192.168.0.254/api/v3')
 app.get_session_token()
 
+def print_contact(app, id):
+    contact = app.get_contact(id)
+    print("Contact Id: %s" % id)
+    print(unicode(contact))
+
 def print_number(app, id):
     number = app.get_number(id)
-    print("Id: %s" % id)
-    print(number)
+    print("Number Id: %s" % id)
+    print(unicode(number))
 
 def print_address(app, id):
     address = app.get_address(id)
-    print("Id: %s" % id)
-    print(address)
+    print("Address Id: %s" % id)
+    print(unicode(address))
 
 def print_email(app, id):
     email = app.get_email(id)
-    print("Id: %s" % id)
-    print(email)
+    print("Email Id: %s" % id)
+    print(unicode(email))
 
 def print_url(app, id):
     url = app.get_url(id)
-    print("Id: %s" % id)
-    print(url)
+    print("Url: %s" % id)
+    print(unicode(url))
+    
+'''
 
 ### Number
 number = app.get_number(100)
@@ -79,13 +86,15 @@ url.type = 'other'
 (success, result) = app.set_url(10,url)
 print_url(app,10)
 
-#
-# Create
-#
+#print(app.delete_contact(496))
+'''
 
+
+#
 # Contact
+#
 
-print(app.delete_contact(496))
+# contact
 
 contact = Contact()
 contact.first_name = "Alain"
@@ -93,9 +102,47 @@ contact.last_name = "Bidon"
 contact.display_name = "BIDON, Alain"
 contact.notes = "Contact de test"
 
+print("\r\nCreate contact :")
+print(contact)
 ncontact = app.new_contact(contact)
-print(ncontact)
+contact_id = ncontact.id
+print_contact(app,contact_id)
 
-print("Delete id: %s" % ncontact.id)
-print(app.delete_contact(ncontact.id))
+# number
+
+number = Number()
+number.contact_id = contact_id
+number.number = '1234567890'
+number.type = 'mobile'
+print("\r\nCreate number :")
+print(number)
+nnumber = app.new_number(number)
+print(nnumber)
+number_id = nnumber.id
+print_number(app,nnumber.id)
+print_contact(app,nnumber.contact_id)
+print_contact(app,contact_id)
+
+number.number = '1234567891'
+number.type = 'fixed'
+print("\r\nCreate number :")
+print(number)
+nnumber = app.new_number(number)
+number1_id = nnumber.id
+print_number(app,number1_id)
+print_contact(app,nnumber.contact_id)
+print_contact(app,contact_id)
+
+'''
+print("\r\nDelete number id: %s" % number1_id)
+print(app.delete_number(number_id))
+print_contact(app,contact_id)
+print("\r\nDelete number id: %s" % number_id)
+print(app.delete_number(number_id))
+print_contact(app,contact_id)
+'''
+
+
+print("\r\nDelete contact id: %s" % contact_id)
+print(app.delete_contact(contact_id))
 
